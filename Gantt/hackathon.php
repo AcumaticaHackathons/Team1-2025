c2e48aac8bf010d08e8e88a81a9f86c624ac00cc
Geoff Tanaka <69860765+geofftanaka@users.noreply.github.com>
2025-01-26 10:28:54 -0800

    Initial setup code

---
+++ Gantt/hackathon.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'URL' => 'https://hackathon.acumatica.com/Alpha/entity/',
+    'INSTANCE' => 'https://hackathon.acumatica.com/Alpha/',
+    'APIVersion'=>'20.200.001',
+    'LOGOUT' => 20
+];
+
